4c9695d8792273f68f9744a792e5b7bfd4f62045
Chris Evans <chrisevans9629@gmail.com>
2022-02-26 15:38:41 -0600

    grid now works

--- src/App.fs
+++ src/App.fs
@@ -7,23 +7,23 @@ open Browser.Types
 open Browser
 
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
+// // Mutable variable to count the number of times we clicked the button
+// let mutable count = 0
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+// // Get a reference to our button and cast the Element to an HTMLButtonElement
+// let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
 
-// Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+// // Register our listener
+// myButton.onclick <- fun _ ->
+//     count <- count + 1
+//     myButton.innerText <- sprintf "You clicked: %i time(s)" count
 
 module Grid =
     type CellValue = PencilMark of int list | Mark of int
     type CellModel = {Column:int; Row:int;Value:CellValue }
 
     let mark (c,r) v = {Column=c;Row=r;Value=Mark(v)}
-
+    let pencil (c,r) v = {Column=c;Row=r;Value=PencilMark(v)}
 module GridGrid = 
     type GridCell = {Column:int; Row:int;Cells:Grid.CellModel list}
     let gridSect (c,r) cells =
@@ -45,6 +45,7 @@ module SampleGrid =
         gridSect (2,0) [
             mark (0,0) 4
             mark (2,2) 3
+            pencil (1,1) [1;2;3]
         ]
         gridSect (0,1) []
         gridSect (1,1) []
@@ -73,15 +74,27 @@ module Canvas =
         ctx.fillStyle <- !^ color
         ctx.fillRect rect
 
-    let stroke (color: string) rect =
+    let stroke lineWidth (color: string) rect =
         let ctx = context
         ctx.strokeStyle <- !^ color
+        ctx.lineWidth <- lineWidth
         ctx.strokeRect rect
 
-    let text txt size (x,y) (color: string) =
+    let line lineWidth (color: string) from lTo =
         let ctx = context
-        ctx.font <- sprintf "%ipx Times New Roman" size
+        ctx.beginPath()
+        ctx.strokeStyle <- !^ color
+        ctx.lineWidth <- lineWidth
+        ctx.moveTo from
+        ctx.lineTo lTo
+        ctx.stroke()
+
+    let text size (x,y) (color: string) txt =
+        let ctx = context
+        ctx.font <- sprintf "%ipx Arial" size
+        ctx.textAlign <- "center"
         ctx.fillStyle <- !^ color
+        ctx.textBaseline <- "middle"
         ctx.fillText (txt, x, y)
 
 
@@ -111,25 +124,49 @@ module Renderer =
         Padding:float
     }
 
+    let drawBackground options =
+        (0., 0., options.Width, options.Height) |> filled (rgb 250 255 255)
+        
+        let drawLines x w clr =
+            line w clr (x,0.) (x,options.Height) 
+            line w clr (0.,x) (options.Width, x)
+
+        for c in 0..9 do
+            let x = float(c) * options.CellSize
+            drawLines x 0. (rgb 0 0 0)
+        
+        
+        for t in 0..3 do
+            let x = float(t) * options.CellSize * 3.
+            drawLines x 10. (rgb 100 0 0)
+
+
     let drawCell (cell:CellModel) ((x,y):float * float) (options:RenderOptions) :unit =
-        ()
+        let cX = float(cell.Column) * options.CellSize + x
+        let cY = float(cell.Row) * options.CellSize + y
+        let middle = (cX+options.CellSize/2.,cY+options.CellSize/2.)
+        match cell.Value with
+        | Mark v -> v.ToString() |> text  45 middle (rgb 0 0 0)
+        | PencilMark values -> 
+            values |> List.sortDescending 
+            |> List.map (fun v -> v.ToString()) 
+            |> List.reduce (fun f s -> s + f) 
+            |> text 20 middle (rgb 0 0 0)
 
     let drawSect (sect:GridCell) options =
         let sectSize = options.CellSize * 3.
         let x = float(sect.Column) * sectSize
         let y = float(sect.Row) * sectSize
 
-        (x+options.Padding,y+options.Padding,x+sectSize-options.Padding,y+sectSize-options.Padding) |> filled (rgb 0 0 0)
+        //(x,y,x+sectSize,y+sectSize) |> filled (rgb x y 0)
 
         for cell in sect.Cells do
             drawCell cell (float(sect.Column) * sectSize, float(sect.Row) * sectSize) options
 
     let render grid options =
-        (0., 0., options.Width, options.Height) |> filled (rgb 255 255 255)
+        drawBackground options
         for sect in grid do
             drawSect sect options
-
-
 
 open Renderer
 
@@ -141,6 +178,6 @@ let options = {CellSize=w/9.; Width=w; Height=h;Padding=10.}
 
 let rec update () =
     render SampleGrid.samGrid options
-    window.setTimeout(update, 1000/60) |> ignore
+    //window.setTimeout(update, 1000/60) |> ignore
 
 update()
